3bcde54e28990a1999fa2da0ed91e9cf2a670baf
GoldingAustin <16907830+GoldingAustin@users.noreply.github.com>
2022-01-02 23:46:09 -0800

    Update object tests again Update name to fix collision of hidden package

--- src/tests/objects.test.ts
+++ src/tests/objects.test.ts
@@ -1,18 +1,34 @@
 import { match } from '../index';
 
+type Update = { score: number; team?: string } | { result: 'win' | 'lose'; team: string } | { quarter: number };
+
+const gameUpdate = (condition: Update) =>
+  match(condition)
+    .when({ score: 5 }, (message) => {
+      console.log(`new score ${message.score} for ${message.team}`);
+      return { score: message.score, team: message.team };
+    })
+    .when({ result: 'win' }, (message) => {
+      console.log(`${message.team} won`);
+      return `${message.team} won`;
+    })
+    .when({ quarter: Number }, (message) => {
+      console.log(`quarter ${message.quarter} ended`);
+      return message.quarter + 1;
+    })
+    .else();
+
 describe('match object', () => {
   it('match object union', () => {
-    type Update = { score: number; team?: string } | { result: 'win' | 'lose'; team: string } | { quarter: number };
+    expect(gameUpdate({ score: 5 })).toEqual({ score: 5, team: undefined });
+    expect(gameUpdate({ result: 'win', team: 'Red' })).toEqual('Red won');
+  });
 
-    const gameUpdate = match<Update>({ score: 5 })
-      .when({ score: 5 }, (message) => {
-        console.log(`new score ${message.score} for ${message.team}`);
-        return { score: message.score, team: message.team };
-      })
-      .when({ result: 'win' }, (message) => console.log(`${message.team} won`))
-      .when({ quarter: Number }, (message) => console.log(`quarter ${message.quarter}`))
-      .else();
+  it('fallback case when condition has no when handler', () => {
+    expect(gameUpdate({ result: 'lose', team: 'Blue' })).toEqual(undefined);
+  });
 
-    expect(gameUpdate).toEqual({ score: 5, team: undefined });
+  it('match quarter type', () => {
+    expect(gameUpdate({ quarter: 2 })).toEqual(3);
   });
 });
